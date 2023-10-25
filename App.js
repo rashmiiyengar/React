c817b8a713fbdca305d531eea4feb82d7c5084b7
Rashmi <rashmiiyengar255@gmail.com>
2023-10-26 00:37:42 +0530

    app design

--- App.js
+++ App.js
@@ -1,33 +1,44 @@
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//    [React.createElement("h1", {}, "Hello im H1"),React.createElement("h2", {}, "Hello im H2")]
-//   )
-// );
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello im H1"),
-    React.createElement("h2", {}, "Hello im H2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hello im H1"),
-    React.createElement("h1", {}, "Hello im H1 second"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading1" },
-  "Hello World from React"
-); //React.createElement returns an objects
+//React Element
+//jsx (transpiled before it reaches to JS Engine) -PARCEL -Babel
+//JSX=> React.createElement=> ReactElement(JS Object) => HTMLElement(Render)
+const jsxHeading = (
+  <div>
+    <h1 className="heading1">hello</h1>
+    <input type="text" />
+    <img 
+      src="https://i.imgur.com/yXOvdOSs.jpg" 
+      alt="Hedy Lamarr" 
+      class="photo"
+    />
+  </div>
+)
 
-console.log(heading);
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
-root.render(parent); //render converts heading object and converts it to h1 tag
 
+root.render(jsxHeading)
 
-//tes comment
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "Hello im H1"),
+//     React.createElement("h2", {}, "Hello im H2"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "Hello im H1"),
+//     React.createElement("h1", {}, "Hello im H1 second"),
+//   ]),
+// ]);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading1" },
+//   "Hello World from React"
+// ); //React.createElement returns an objects
+
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(parent);
+// root.render(parent); //render converts heading object and converts it to h1 tag
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 React js
+
+#Parcel 
+-Dev Build
+-Local Server
+-HMR = Hot Module Replacement
+-File watching algorithm - written in c++
+-Faster Build -Caching
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistant Hashing
+-Differential Bundling
+-Diagnostic
+-Tree shaking- Remove unused code
