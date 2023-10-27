11fdf9a735d8b290be29df61d966a512e607b7d6
Rashmi <rashmiiyengar255@gmail.com>
2023-10-28 02:50:37 +0530

    Added Shimmer Ui to Cards

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,45 @@
 import RestrauntCard from "./RestrauntCard";
-import { data as swiggyRestaurantList } from "../../mocks/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+//import { data as swiggyRestaurantList } from "../../mocks/mockData";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   //State Variable = Powerful variable
   //React Hooks- useState
-  const arr = useState(swiggyRestaurantList);
-  console.log(arr)
-  const [listOfRestraunt, setListOfRestraunt] = useState(swiggyRestaurantList);
 
-  return (
+  const [listOfRestraunt, setListOfRestraunt] = useState([]);
+
+  useEffect(() => {
+    console.log("rash");
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const jsonResponseData = await data.json();
+    console.log(jsonResponseData);
+    setListOfRestraunt(
+      //Optional Chaining
+      jsonResponseData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+  //Conditional Rendering
+
+  return listOfRestraunt.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
-        <button
+        <button 
           className="filter-btn btn btn-outline-success"
           onClick={() => {
             let filteredList = listOfRestraunt.filter(
-              (res) => res.info.avgRating > 4.4
+              (res) => res.info.avgRating > 4.2
             );
             setListOfRestraunt(filteredList);
             console.log(filteredList);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import Logo from "../../logo.png";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnNameReact, setbtnNameReact] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container ">
@@ -12,6 +15,14 @@ const Header = () => {
           <li className="text-success">AboutUs</li>
           <li className="text-success">ContactUs</li>
           <li className="text-success">Cart</li>
+          <button
+            className="btn btn-outline-success"
+            onClick={() => {
+              setbtnNameReact("Logout");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
